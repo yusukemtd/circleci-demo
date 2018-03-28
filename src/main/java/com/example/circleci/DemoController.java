aae285a612acee1193b47202b36dcdb0900f6dd7
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-03-29 01:14:30 +0900

    first commit.

---
+++ src/main/java/com/example/circleci/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.circleci;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "Hello Demo.";
+    }
+}
